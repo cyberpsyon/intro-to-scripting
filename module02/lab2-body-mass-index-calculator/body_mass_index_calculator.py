a4f26d08e8223a118060bd08c54a3910e023feaa
cyberpsyon <bjmickens@gmail.com>
2025-11-02 12:00:16 -0600

    Added input validation to body_mass_index_calculator

--- module02/lab2-body-mass-index-calculator/body_mass_index_calculator.py
+++ module02/lab2-body-mass-index-calculator/body_mass_index_calculator.py
@@ -5,8 +5,10 @@
 import bmi_utils as b
 
 def main():
+    """ Main entry point of the program."""
     print("Welcome to the BMI Calculator!")
     while True:
+        # Display menu
         print("\nChoose an option:")
         print("1) Convert weight in pounds (lbs) to kilograms (kg)")
         print("2) Convert height in inches (in) to meters (m)")
@@ -16,20 +18,49 @@ def main():
 
         choice = input("Enter your choice (1-5): ")
 
+        # Convert pounds to kilograms
         if choice == "1":
-            pounds = float(input("Enter your weight in pounds (lbs): "))
-            print(f"Your weight in kilograms (kg) is {b.convert_lbs_to_kg(pounds):.2f}.")
+            try:
+                pounds = float(input("Enter your weight in pounds (lbs): "))
+                if pounds <=0:
+                    raise ValueError("Weight must be greater than zero")
+                else:
+                    print(f"Your weight in kilograms (kg) is {b.convert_lbs_to_kg(pounds):.2f}.")
+            except ValueError:
+                print("Please enter a valid number.")
+        # Convert inches to meters
         elif choice == "2":
-            inches = float(input("Enter your height in inches (in): "))
-            print(f"Your height in meters (m) is {b.convert_inches_to_meters(inches):.2f}.")
+            try:
+                inches = float(input("Enter your height in inches (in): "))
+                if inches <=0:
+                    raise ValueError("Height must be greater than zero")
+                else:
+                    print(f"Your height in meters (m) is {b.convert_inches_to_meters(inches):.2f}.")
+            except ValueError:
+                print("Please enter a valid number.")
+        # Calculate BMI
         elif choice == "3":
-            weight = float(input("Enter your weight in kilograms (kg): "))
-            height = float(input("Enter your height in meters (m): "))
-            bmi = b.calculate_bmi(weight, height)
-            b.output_bmi_stats(bmi)
+            try:
+                weight = float(input("Enter your weight in kilograms (kg): "))
+                height = float(input("Enter your height in meters (m): "))
+                if weight <= 0 or height <= 0:
+                    raise ValueError("Height and weight must be greater than zero")
+                else:
+                    bmi = b.calculate_bmi(weight, height)
+                    b.output_bmi_stats(bmi)
+            except ValueError:
+                print("Please enter a valid number.")
+        # Calculate optimal weight range
         elif choice == "4":
-            height = float(input("Enter your height in meters (m): "))
-            b.calculate_optimal_weight(height)
+            try:
+                height = float(input("Enter your height in meters (m): "))
+                if height <= 0:
+                    raise ValueError("Height must be greater than zero")
+                else:
+                    b.calculate_optimal_weight(height)
+            except ValueError:
+                print("Please enter a valid number.")
+        # Exit the program
         elif choice == "5":
             print("Thank you for using BMI Calculator!")
             break
